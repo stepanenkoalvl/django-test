e1c78acbae2d4b71dfb623302da5dd1a434dcf5e
Stepanenko Oleksandr <stepanenko.alvl@gmail.com>
2016-05-25 19:27:41 +0300

    DB behaviour was added, test cases were fixed, tests pass correct

--- src/test1/tests.py
+++ src/test1/tests.py
@@ -1,65 +1,73 @@
 from django.test import TestCase
 from django.test import Client
 from django.core.urlresolvers import reverse
+from models import Somebody
+from datetime import datetime
 
 # Create your tests here.
 
 
 class MainViewTests(TestCase):
+    
+    def setUp(self):
+        Somebody.objects.create(firstName=u'Oleksandr', lastName=u'Stepanenko',
+            birthDate=datetime.strptime('07/19/1982', '%m/%d/%Y'), BIO=u'???',
+            email=u'stepanenko.alvl@gmail.com', jabber=u'stepanenkoav@42cc.co',
+            skype=u'stepanenko.alvl')
+
     def test_index_view_with_first_name(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "Oleksandr", status_code=200)
+        self.assertContains(response, "Oleksandr")
 
     def test_index_view_with_last_name(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "Stepanenko", status_code=200)
+        self.assertContains(response, "Stepanenko")
     
     def test_index_view_with_BIO(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "???", status_code=200)
+        self.assertContains(response, "???")
 
     def test_index_view_with_birth_date(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "07/19/1982", status_code=200)
+        self.assertContains(response, "07/19/1982")
 
     def test_index_view_with_email(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "stepanenko.alvl@gmail.com", status_code=200)
+        self.assertContains(response, "stepanenko.alvl@gmail.com")
 
     def test_index_view_with_jabber(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "stepanenkoav@42cc.co", status_code=200) 
+        self.assertContains(response, "stepanenkoav@42cc.co") 
 
     def test_index_view_with_skype(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertContains(response, "stepanenko.alvl", status_code=200)
+        self.assertContains(response, "stepanenko.alvl")
 
-class MainViewDBTests(TestCase):
     def test_index_view_first_name_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['first_name'], 'Oleksandr')
+        self.assertEqual(response.context['person'].firstName, 'Oleksandr')
 
     def test_index_view_last_name_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['last_name'], 'Stepanenko')
+        self.assertEqual(response.context['person'].lastName, 'Stepanenko')
 
     def test_index_view_BIO_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['BIO'], '???')
+        self.assertEqual(response.context['person'].BIO, '???')
 
     def test_index_view_birth_date_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['birth_date'], '07/19/1982')
+        self.assertEqual(response.context['person'].birthDate.isoformat(), '1982-07-19')
 
     def test_index_view_email_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['email'], 'stepanenko.alvl@gmail.com')
+        self.assertEqual(response.context['person'].email, 'stepanenko.alvl@gmail.com')
 
     def test_index_view_jabber_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['jabber'], 'stepanenkoav@42cc.co')
+        self.assertEqual(response.context['person'].jabber, 'stepanenkoav@42cc.co')
 
     def test_index_view_skype_from_DB(self):
         response = self.client.get(reverse('test1:index'))
-        self.assertQuerysetEqual(response.context['skype'], 'stepanenko.alvl')
+        self.assertEqual(response.context['person'].skype, 'stepanenko.alvl')
 
--- src/test1/views.py
+++ src/test1/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from models import Somebody
 # Create your views here.
 
 def index(request):
-    return render(request, 'test1/index.html', {})
+    first_person = get_object_or_404(Somebody, pk=1)
+    return render(request, 'test1/index.html', {'person':first_person})
